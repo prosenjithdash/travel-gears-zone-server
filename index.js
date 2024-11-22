7553e1b094c85b333902c01708b2d411a7427798
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2024-11-22 17:19:14 +0600

    Products sorting , searching work is running

--- index.js
+++ index.js
@@ -7,8 +7,42 @@ const app = express()
 const port = process.env.PORT || 8000;
 
 // middleware declare
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    optionsSuccessStatus:200,
+}))
 app.use(express.json())
+
+
+
+
+// token verification
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.send({message:"No Token"})
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.send({message:"Invalid Token"})
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+// verify seller
+const verifySeller = async (req,res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email }
+    const user = await userCollection.findOne(query)
+    if (user?.role !== "seller") {
+        return res.send({message:"Forbidden access"})
+    }
+    next();
+}
+
 
 // mongodb
 
@@ -34,7 +68,16 @@ const dbConnect = async () => {
         client.connect();
         console.log('Database connected successfully');
 
-        // insert user
+
+         // get user
+        app.get("/user/:email", async (req, res) => {
+            const query = { email: req.params.email };
+            const user = await userCollection.findOne(query);
+            res.send(user)
+        });
+
+
+        // insert user data
         app.post("/users", async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
@@ -47,6 +90,55 @@ const dbConnect = async () => {
             const result = await userCollection.insertOne(user);
             res.send(result);
         });
+
+
+        // Add Product API
+        app.post("/add-products",verifyJWT,verifySeller, async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+
+        // get products from Product API
+        app.get("/all-products", async (req, res) => {
+            
+            // name searching
+            // sort by price
+            // filter by category
+            //filter by brand
+
+            const { name, sort, category, brand } = req.query
+            
+            const query = {}
+
+            if (title) {
+                query.title = { $regex: title, $options: "i" }
+            }
+
+            if (category) {
+                query.category = { $regex: category, $options: "i" }
+            }
+
+            if (brand) {
+                query.brand = brand;
+            }
+
+            const sortOption = sort === 'asc' ? 1 : -1
+            
+            const products = await productCollection
+                .find(query)
+                .sort({ price: sortOption })
+                .toArray();
+            res.json(products);
+
+
+
+
+        });
+
+
+
+
     } catch (error) {
         console.log(error.name, error.message);
     }
